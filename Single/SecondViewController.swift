929f22a391227c24e901e8696bd64ef08befaa7a
admin <admin@admin-no-MacBook-Pro.local>
2016-08-18 23:51:04 +0900

    refs #3 add playcard data with png and first view

--- Single/SecondViewController.swift
+++ Single/SecondViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+
+    
+    private var myImageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        // UIImageViewを作成する.
+        myImageView = UIImageView(frame: CGRectMake(0,0,100,120))
+        
+        // 表示する画像を設定する.
+        let myImage = UIImage(named: "png/c01.png")
+        
+        // 画像をUIImageViewに設定する.
+        myImageView.image = myImage
+        
+        // 画像の表示する座標を指定する.
+        myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: 200.0)
+        
+        // UIImageViewをViewに追加する.
+        self.view.addSubview(myImageView)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
     
